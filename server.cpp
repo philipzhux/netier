7c3557ff534a3f568ad7327e4f64593c855a618f
Philip <me@cyzhu.dev>
2022-11-20 09:29:23 -0800

    implement server class apis

--- server.cpp
+++ server.cpp
@@ -5,3 +5,33 @@
  */
 
 #include "server.h"
+
+Server::Server(Address addr) : __addr(addr)
+{
+    __acceptor = std::make_unique<Acceptor>(__addr, std::bind(&Server::contextCreator, this));
+    __threadpool = std::make_unique<ThreadPool>();
+    size_t size = __threadpool->getSize();
+    for (size_t i = 0; i < size; i++)
+        __reactors.emplace_back(Reactor{});
+}
+
+void Server::serve()
+{
+    size_t size = __threadpool->getSize();
+    for (size_t i = 0; i < size; i++)
+        __threadpool->asyncRunJob(std::bind(&Reactor::loop,&__reactors[i]));
+    
+
+}
+
+
+
+void Server::setOnConn(std::function<Context *(void)> onConn)
+{
+    __onConn = std::move(onConn);
+}
+
+void Server::setOnRecv(std::function<Context *(void)> onRecv)
+{
+    __onRecv = std::move(onRecv);
+}
--- server.h
+++ server.h
@@ -9,21 +9,27 @@
 #include "acceptor.h"
 #include <vector>
 #include <unordered_map>
+#include <string>
 class Server
 {
 public:
+    // Server(std::string host, uint16_t port);
     Server(Address addr);
+    ~Server();
     void setOnConn(std::function<Context *(void)>);
     void setOnRecv(std::function<Context *(void)>);
-    void start();
-    Context *contextCreator(int fd);
+    void serve();
+    Context *contextCreator(int fd, Address addr);
+    Reactor* schedule(int fd);
     void contextDestroyer(int fd);
-    ~Server();
+   
 
 private:
     Address __addr;
     std::vector<Reactor> __reactors;
     std::unique_ptr<ThreadPool> __threadpool;
     std::unique_ptr<Acceptor> __acceptor;
-    std::unordered_map<int, std::unique_ptr<Context>> contextes; // a context is binded with a reactor at create time
+    std::unordered_map<int, std::unique_ptr<Context>> __contextes; // a context is binded with a reactor at create time
+    std::function<Context *(void)> __onConn;
+    std::function<Context *(void)> __onRecv;
 };
